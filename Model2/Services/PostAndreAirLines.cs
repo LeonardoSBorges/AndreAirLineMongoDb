a2a031eecde6643a352dbb72a3e3858598fdc120
Leonardo Borges <ldssborges@hotmail.com>
2022-04-14 21:22:17 -0300

    Foi inserido o rabbitMQ

--- Model2/Services/PostAndreAirLines.cs
+++ Model2/Services/PostAndreAirLines.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,21 @@ namespace ModelShare.Services
         public static async Task PostLog(LogDTO newLogDTO)
         {
             HttpResponseMessage response = await client.PostAsJsonAsync<LogDTO>($"https://localhost:44364/api/Log/", newLogDTO);
+        }
+
+        public static async Task Add(Log log)
+        {
+            try
+            {
+                if (client.BaseAddress == null) client.BaseAddress = new Uri("https://localhost:44309/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                await client.PostAsJsonAsync("api/Logs", log);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
         }
     }
 }
